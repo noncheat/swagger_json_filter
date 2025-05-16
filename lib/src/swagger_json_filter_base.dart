ea16381d7350213be45daa9d47868d0849a27ff2
quyenvsp <quyenvsp@gmail.com>
2025-05-16 17:00:20 +0700

    feat: add option whiteListDefinitions and addNullable

--- lib/src/swagger_json_filter_base.dart
+++ lib/src/swagger_json_filter_base.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'dart:convert';
 
 import 'package:flat/flat.dart' show flatten;
@@ -10,21 +12,42 @@ class SwaggerJsonFilterOptions {
     this.excludeTags,
     this.includePaths,
     this.excludePaths,
+    this.whiteListDefinitions,
     this.clearRequired = true,
+    this.addNullable = true,
   });
 
+  /// eg: `[RegExp(r'^Include Tag.*')]`
   List<RegExp>? includeTags;
+
+  /// eg: `[RegExp(r'^Exclude Tag$')]`
   List<RegExp>? excludeTags;
+
+  /// eg: `[RegExp(r'^/api/v1/app/.*')]`
   List<RegExp>? includePaths;
+
+  /// eg: `[RegExp(r'(.*?)')]`
   List<RegExp>? excludePaths;
+
+  /// `SwaggerJsonFilter.filter` will remove Definitions which unused
+  ///
+  /// Use this for keep Definitions. eg: `['components.schemas.KeycloakError']`
+  List<String>? whiteListDefinitions;
+
+  /// Should clear `required` fields or not
   bool clearRequired;
+
+  /// Should add `x-nullable` and `nullable` or not
+  bool addNullable;
 }
 
 class SwaggerJsonFilter {
+  /// Allocates a new SwaggerJsonFilter for the specified [options].
   const SwaggerJsonFilter({
     required this.options,
   });
 
+  /// Filter options
   final SwaggerJsonFilterOptions options;
 
   /// Filter swagger json paths, return filtered json string from input [jsonString]
@@ -45,16 +68,60 @@ class SwaggerJsonFilter {
     for (final path in paths.keys) {
       _searchReferencesFor(json, paths[path], whiteList);
     }
+    _whiteListDefinitions(json, whiteList);
 
     // Clean
     json = _clearMismatchedElements(json, definitionsList, whiteList);
-    // Clear required properties because build_value not support required fields
-    // https://github.com/google/built_value.dart/issues/1050
-    if (options.clearRequired) json = _clearRequiredProperties(json, whiteList);
+
+    final definitionsWithProperties = _findDefinitions(json);
+
+    // Rewrite process
+    json = _forEachDefinitions(
+      json,
+      definitionsWithProperties,
+      _rewriteDefinitions,
+    );
 
     final result = _filterJson(json);
 
     return result;
+  }
+
+  Iterable<String> _findDefinitions(Map<String, dynamic> json) {
+    final flattenDefinitions = flatten(json);
+    final result = <String, dynamic>{};
+    for (final key in flattenDefinitions.keys) {
+      // Version 2
+      if (key.startsWith('definitions.')) {
+        final defName = key.split('.').getRange(0, 2).join('.');
+        result[defName] = true;
+      }
+      // Version 3
+      if (key.startsWith('components.schemas.')) {
+        final defName = key.split('.').getRange(0, 3).join('.');
+        result[defName] = true;
+      }
+    }
+    return result.keys;
+  }
+
+  void _whiteListDefinitions(
+    Map<String, dynamic> json,
+    List<String> whiteList,
+  ) {
+    if (options.whiteListDefinitions?.isNotEmpty == true) {
+      for (final defName in options.whiteListDefinitions ?? <String>[]) {
+        final nestedDefinition = gato.get(json, defName);
+        if (nestedDefinition is Map &&
+            _shouldSaveReference(defName, whiteList)) {
+          _saveReference(defName, whiteList);
+        } else {
+          print(
+            '\x1B[33m[WARNING]\x1B[0m whiteListDefinitions: reference $defName is not Map or already saved',
+          );
+        }
+      }
+    }
   }
 
   /// Apply filter remove unwanted data
@@ -186,24 +253,78 @@ class SwaggerJsonFilter {
     return json;
   }
 
-  /// Clear required properties
-  Map<String, dynamic> _clearRequiredProperties(
+  Map<String, dynamic> _forEachDefinitions(
     Map<String, dynamic> json,
-    List<String> whiteList,
+    Iterable<String> definitions,
+    void Function(Map) callback,
   ) {
-    for (final key in whiteList) {
+    for (final key in definitions) {
       final value = gato.get<Map>(json, key);
-      if (value?['required'] is List &&
-          value?['required'].isNotEmpty &&
-          value?['properties'] is Map) {
-        value?.remove('required');
+      if (value != null) {
+        // foreach object and check ['type'] is String
+        // instead of ['properties'] is Map because enum do not have ['properties']
+        // callback should check if have ['properties'] before process
+        if (value['allOf'] is List) {
+          for (int i = 0; i < value['allOf'].length; i++) {
+            if (value['allOf'][i]['type'] is String) {
+              callback(value['allOf'][i]);
+            }
+          }
+        } else if (value['type'] is String) {
+          callback(value);
+        }
+        json = gato.set(json, key, value);
       }
-      json = gato.set(json, key, value);
     }
     return json;
   }
 
+  /// Rewrite properties
+  void _rewriteDefinitions(Map value) {
+    // Clear required properties because built_value not support required fields
+    // https://github.com/google/built_value.dart/issues/1050
+    if (options.clearRequired) {
+      _clearRequiredProperties(value);
+    }
+    // Add nullable for support Dart null-safety,
+    // using with [TryParsePlugin] will avoid cast type exception when deserialize
+    if (options.addNullable) {
+      _addNullableProperties(value);
+    }
+    // Common rewrite
+    // Support enum x-enumNames
+    if (value['x-enum-varnames'] == null && value['x-enumNames'] != null) {
+      value['x-enum-varnames'] = value['x-enumNames'];
+    }
+  }
+
+  /// Clear required properties
+  void _clearRequiredProperties(Map value) {
+    if (value['required'] is List) {
+      value.remove('required');
+    }
+  }
+
+  /// Add nullable properties
+  void _addNullableProperties(Map value) {
+    for (final keyProperties in (value['properties'] as Map?)?.keys ?? []) {
+      // should use both nullable and x-nullable for support multiple version
+      value['properties'][keyProperties]['nullable'] = true;
+      value['properties'][keyProperties]['x-nullable'] = true;
+      // property use $ref
+      if (value['properties'][keyProperties][r'$ref'] is String) {
+        final ref = value['properties'][keyProperties][r'$ref'] as String;
+        // OpenAPI version 2.0 do not use anyOf NullType, will lead to problem JsonObject
+        // So for version <= 3.0 use allOf with nullable, x-nullable
+        value['properties'][keyProperties]['allOf'] = [
+          {r'$ref': ref},
+        ];
+        value['properties'][keyProperties].remove(r'$ref');
+      }
+    }
+  }
+
   String _filterJson(Map inputJson) {
-    return JsonEncoder.withIndent('  ').convert(inputJson);
+    return const JsonEncoder.withIndent('  ').convert(inputJson);
   }
 }
